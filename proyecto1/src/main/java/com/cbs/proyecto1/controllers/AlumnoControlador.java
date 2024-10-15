9335003938b1f25dbcdf5db7a2843feacf91dfd4
unknown <alumnofp@FPB09.CALASANZSALAMANCA.local>
2024-10-15 12:43:23 +0200

    bruno

--- proyecto1/src/main/java/com/cbs/proyecto1/controllers/AlumnoControlador.java
+++ proyecto1/src/main/java/com/cbs/proyecto1/controllers/AlumnoControlador.java
@@ -1,10 +1,12 @@
 package com.cbs.proyecto1.controllers;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cbs.proyecto1.models.AlumnoDto;
@@ -17,7 +19,8 @@ public class AlumnoControlador {
 	public AlumnoControlador() {
 
 		listaAlumnos = new ArrayList<AlumnoDto>();
-		listaAlumnos.add(new AlumnoDto("Pepe", "Roca"));
+
+		listaAlumnos.add(new AlumnoDto(1, "Pepe", "Roca", "77770055l"));
 
 	}
 
@@ -26,10 +29,26 @@ public class AlumnoControlador {
 		return listaAlumnos;
 	}
 
-	@GetMapping("/AlumnoDto/add")
-	public List<AlumnoDto> ListaAlumnos() {
+	// @PostMapping: Define una ruta para que este método maneje solicitudes HTTP
+	// POST en la URL
+	@PostMapping("/AlumnoDto/add")
+	public List<AlumnoDto> add(@RequestBody AlumnoDto nuevoAlumno) {
+		// *@RequestBody: Aquí el nuevo alumno se obtiene del cuerpo de la solicitud.
 
-		listaAlumnos.add(new AlumnoDto("Pepe", "Roca"));
+		listaAlumnos.add(nuevoAlumno);
+		return listaAlumnos;
+
+	}
+
+	/*
+	 * Identificador nuevo data encontrar el objeto a modificar que NO exista
+	 * Actualizr Objeto data guardar sin errores AVISAR
+	 */
+
+	// ACTUALIZAR:
+	@PutMapping("/AlumnoDto/update")
+	public List<AlumnoDto> update(@RequestBody AlumnoDto AlumnoModificado) {
+
 		return listaAlumnos;
 
 	}
--- proyecto1/src/main/java/com/cbs/proyecto1/models/AlumnoDto.java
+++ proyecto1/src/main/java/com/cbs/proyecto1/models/AlumnoDto.java
@@ -1,21 +1,36 @@
 package com.cbs.proyecto1.models;
 
-import java.util.List;
 
 public class AlumnoDto {
 
-	
+	private int id;
+	private String dni;
 	private String nombre;
 	private String apellidos;
 
-
-	public AlumnoDto(String nombre, String apellidos) {
+	public AlumnoDto(int id, String dni, String nombre, String apellidos) {
 		super();
+		this.id = id;
+		this.dni = dni;
 		this.nombre = nombre;
 		this.apellidos = apellidos;
-		
-		
-		
+	}
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
 	}
 
 	public String getNombre() {
@@ -34,17 +49,10 @@ public class AlumnoDto {
 		this.apellidos = apellidos;
 	}
 
+	
 	@Override
 	public String toString() {
-		return "AlumnoDto [nombre=" + nombre + ", apellidos=" + apellidos + "]";
+		return "AlumnoDto [id=" + id + ", dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + "]";
 	}
 
-	
-
-	
-
-	
-	
 }
-
-	
