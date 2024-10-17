9d6500313ff1ddeb9251421f46cf9319d25e90cf
unknown <alumnofp@FPB09.CALASANZSALAMANCA.local>
2024-10-17 10:11:57 +0200

    Update y Delete

--- proyecto1/src/main/java/com/cbs/proyecto1/controllers/AlumnoControlador.java
+++ proyecto1/src/main/java/com/cbs/proyecto1/controllers/AlumnoControlador.java
@@ -3,6 +3,8 @@ package com.cbs.proyecto1.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +23,8 @@ public class AlumnoControlador {
 		listaAlumnos = new ArrayList<AlumnoDto>();
 
 		listaAlumnos.add(new AlumnoDto(1, "Pepe", "Roca", "77770055l"));
+		listaAlumnos.add(new AlumnoDto(2, "00002", "Javier", "Roca"));
+		listaAlumnos.add(new AlumnoDto(3, "00003", "María", "Roca"));
 
 	}
 
@@ -40,21 +44,92 @@ public class AlumnoControlador {
 
 	}
 
-	/*
-	 * Identificador nuevo data 
-	 * encontrar el objeto a modificar 
-	 * que NO exista
-	 * Actualizr Objeto data 
-	 * guardar sin errores 
-	 * 
-	 * AVISAR
-	 */
+	// Encontrar el objeto a modificar
+	// Error si no existe
+	// Actualizar datos del objeto
+	// Guardar
+	// Sin errores de guardado
+	// Avisar
 
 	// ACTUALIZAR:
 	@PutMapping("/AlumnoDto/update")
-	public List<AlumnoDto> update(@RequestBody AlumnoDto AlumnoModificado) {
+	public String update(@RequestBody AlumnoDto AlumnoModificado) {
+
+		for (AlumnoDto alumno : listaAlumnos) {
+			if (alumno.getId() == alumno.getId()) {
+
+				return "Alumno actualizado";
+			}
+		}
+		return "Alumno no encontrado.";
+	}
+
+	@DeleteMapping("/AlumnoDto/deleteFull")
+
+	public List<AlumnoDto> deleteFull(@RequestBody AlumnoDto studentDelete) {
+
+		// Opción 1: guardar posición
+		// int position = -1;
+
+		// Opción 2: guardar en objeto
+
+		AlumnoDto studentAux = null;
+		for (AlumnoDto student : listaAlumnos) {
+
+			if (studentDelete.getDni().equals(student.getDni())) {
+				
+				// Opción 1: guardar posición
+				// position = listaAlumnos.indexOf(student);
+				
+				// Opción 2: guardar en objeto
+				studentAux = student;
+			}
+		}
+
+		// Opción 1: guardar posición
+		// listaAlumnos.remove(position);
+		
+		// Opción 2: guardar en objeto
+		listaAlumnos.remove(studentAux);
+		
+		// Opción 3: (Intermedia remix de las dos anteriores)
+		// int position = listaAlumnos.indexOf(studentAux);
+		// listaAlumnos.remove(position);
 
 		return listaAlumnos;
+	}
+
+	@DeleteMapping("/student/delete")
+
+	public ResponseEntity<?> delete(@RequestBody AlumnoDto studentDelete) {
+
+		AlumnoDto studentAux = null;
+
+		try {
+
+			for (AlumnoDto student : listaAlumnos) {
+
+				if (studentDelete.getDni().equals(student.getDni())) {
+
+					studentAux = student;
+				}
+			}
+
+			if (null != studentAux) {
+				listaAlumnos.remove(studentAux);
+
+				return ResponseEntity.ok(listaAlumnos);
+			} else {
+				return ResponseEntity.badRequest().body("Usuario no encontrado");
+			}
+
+		} catch (Exception ex) {
+
+			return ResponseEntity.internalServerError().body("error al procesar la eliminación");
+
+		}
+		
 
 	}
+
 }
